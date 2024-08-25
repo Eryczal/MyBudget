b7523768643e9952d5e1ab0dee1c0c85988149b1
Eryczal <79263592+Eryczal@users.noreply.github.com>
2024-08-25 22:05:51 +0200

    Display Budget in Chart

--- app/(tabs)/budget.tsx
+++ app/(tabs)/budget.tsx
@@ -1,78 +1,62 @@
 import { View } from "@/components/Themed";
-import { StatusBar } from "expo-status-bar";
-import { Dimensions, ScrollView, StyleSheet } from "react-native";
+import { useState } from "react";
+import { Dimensions, StyleSheet, TouchableWithoutFeedback } from "react-native";
+import { LineChart } from "react-native-chart-kit";
 
-import { Card, Icon, ProgressBar, Text } from "react-native-paper";
+import { Text } from "react-native-paper";
 
 const window = Dimensions.get("window");
 
+const chartData = {
+    labels: ["01-07", "08-15", "16-23", "24-30"],
+    datasets: [
+        {
+            data: [3200, 3100, 3500, 3000],
+        },
+    ],
+};
+
+const chartConfig = {
+    backgroundColor: "#e26a00",
+    backgroundGradientFrom: "#fb8c00",
+    backgroundGradientTo: "#ffa726",
+    decimalPlaces: 0,
+    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    style: {
+        borderRadius: 16,
+    },
+    propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        stroke: "#ffa726",
+    },
+};
+
 export default function BudgetScreen() {
+    const [fromZero, setFromZero] = useState<boolean>(true);
+    const changeChart = () => {
+        setFromZero(!fromZero);
+    };
+
     return (
         <View style={styles.container}>
-            <ScrollView horizontal style={styles.scrollContainer}>
-                <Card mode="elevated" style={styles.card}>
-                    <Card.Content>
-                        <Text variant="titleLarge">Budżet</Text>
-                        <Text variant="bodyMedium">Dostępne pieniądze: 0zł</Text>
-                        <Text variant="bodyMedium">W tym miesiącu: 0zł</Text>
-                    </Card.Content>
-                </Card>
-                <Card mode="elevated" style={styles.card}>
-                    <Card.Content>
-                        <Text variant="titleLarge">Oszczędności</Text>
-                        <Text variant="bodyMedium">Zaoszczędzono: 120zł</Text>
-                        <Text variant="bodyMedium">W tym miesiącu: 0zł</Text>
-                    </Card.Content>
-                </Card>
-            </ScrollView>
-            <Card mode="contained">
-                <Text variant="headlineLarge" style={styles.header}>
-                    Twoje cele
-                </Text>
-                <Card mode="elevated" style={styles.bought}>
-                    <View style={styles.boughtContainer}>
-                        <View style={styles.boughtView}>
-                            <Icon source="piggy-bank-outline" size={26} />
-                            <Text variant="bodyLarge" style={styles.boughtName}>
-                                Rower
-                            </Text>
-                        </View>
-                        <View>
-                            <Text variant="bodyLarge">30zł / 3000zł</Text>
-                        </View>
-                    </View>
-                    <ProgressBar progress={0.01} color="#d9c222" style={styles.bar} />
-                </Card>
-                <Card mode="elevated" style={styles.bought}>
-                    <View style={styles.boughtContainer}>
-                        <View style={styles.boughtView}>
-                            <Icon source="piggy-bank-outline" size={26} />
-                            <Text variant="bodyLarge" style={styles.boughtName}>
-                                Rower
-                            </Text>
-                        </View>
-                        <View>
-                            <Text variant="bodyLarge">300zł / 3000zł</Text>
-                        </View>
-                    </View>
-                    <ProgressBar progress={0.1} color="#d9c222" style={styles.bar} />
-                </Card>
-                <Card mode="elevated" style={styles.bought}>
-                    <View style={styles.boughtContainer}>
-                        <View style={styles.boughtView}>
-                            <Icon source="piggy-bank-outline" size={26} />
-                            <Text variant="bodyLarge" style={styles.boughtName}>
-                                Rower
-                            </Text>
-                        </View>
-                        <View>
-                            <Text variant="bodyLarge">1500zł / 3000zł</Text>
-                        </View>
-                    </View>
-                    <ProgressBar progress={0.5} color="#d9c222" style={styles.bar} />
-                </Card>
-            </Card>
-            <StatusBar style={"dark"} />
+            <Text variant="headlineMedium" style={styles.header}>
+                Sierpień 2024
+            </Text>
+            <TouchableWithoutFeedback onPress={changeChart}>
+                <View>
+                    <LineChart
+                        data={chartData}
+                        width={window.width * 0.94}
+                        height={220}
+                        chartConfig={chartConfig}
+                        yAxisSuffix="zł"
+                        fromZero={fromZero}
+                        style={styles.chart}
+                    />
+                </View>
+            </TouchableWithoutFeedback>
         </View>
     );
 }
@@ -82,45 +66,12 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#fafafa",
     },
-    scrollContainer: {
-        flexDirection: "row",
-        flexGrow: 0,
-    },
-    card: {
-        width: window.width * 0.8,
-        margin: 10,
-        padding: 20,
-        backgroundColor: "#fff",
-    },
     header: {
         margin: 20,
         textAlign: "center",
     },
-    bought: {
-        padding: 12,
-        paddingVertical: 24,
-        backgroundColor: "#fff",
-        width: window.width * 0.9,
-        margin: "auto",
-        marginVertical: 5,
-    },
-    boughtContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        marginBottom: 16,
-    },
-    boughtView: {
-        flexDirection: "row",
-        alignItems: "center",
-    },
-    boughtName: {
-        marginLeft: 6,
-    },
-    bar: {
-        backgroundColor: "#ebd777",
-    },
-    icon: {
-        margin: 0,
+    chart: {
+        borderRadius: 16,
+        alignSelf: "center",
     },
 });
